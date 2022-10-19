e85f0004b7d0a3ba83609ff65e4eac0fc94cd830
Sandun-Induranga <sandutharu40@gmail.com>
2022-10-19 22:35:10 +0530

    Content Updated

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -1,10 +1,14 @@
 const deg = 6;
 
-const hour = $(".hour");
-const minute = $(".min");
-const sec = $(".sec");
+const hour = $("section:nth-child(1)");
+const minute = $("section:nth-child(2)");
+const sec = $("section:nth-child(3)");
 
 let date = new Date();
 let hh = date.getHours() * 30;
 let mm = date.getMinutes() * deg;
 let ss = date.getSeconds() * deg;
+
+hour.css('transform', `rotateZ(${hh + (mm/12)}deg)`);
+minute.css('transform', `rotateZ(${mm}deg)`);
+sec.css('transform', `rotateZ(${ss}deg)`);
