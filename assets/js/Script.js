1b42a78ea0b35cdf0ec6f30ae2e8f8950e106298
Sandun-Induranga <sandutharu40@gmail.com>
2022-10-19 21:57:49 +0530

    Content Updated

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -3,3 +3,8 @@ const deg = 6;
 const hour = $(".hour");
 const minute = $(".min");
 const sec = $(".sec");
+
+let date = new Date();
+let hh = date.getHours() * 30;
+let mm = date.getMinutes() * deg;
+let ss = date.getSeconds() * deg;
